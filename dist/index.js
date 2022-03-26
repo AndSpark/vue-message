31b399101f1e02c03e7ff515e7487a7282f3092e
hibana <421786477@qq.com>
2022-03-26 16:25:50 +0800

    package

--- dist/index.js
+++ dist/index.js
@@ -1,46 +1,52 @@
-import { h, render } from 'vue';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.message = void 0;
+const vue_1 = require("vue");
 const MessageContainerPrefixId = `gls-message-container`;
 function FaSolidTimesCircle(props) {
-    return h('svg', Object.assign({ width: '1em', height: '1em', viewBox: '0 0 512 512', style: { color: '#ff6347' } }, props), [
-        h('path', {
+    return (0, vue_1.h)('svg', Object.assign({ width: '1em', height: '1em', viewBox: '0 0 512 512', style: { color: '#ff6347' } }, props), [
+        (0, vue_1.h)('path', {
             d: 'M256 8C119 8 8 119 8 256s111 248 248 248s248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65l-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7l65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z',
             fill: 'currentColor'
         })
     ]);
 }
 const LoadingIcon = (props) => {
-    return h('svg', Object.assign({ viewBox: '0 0 512 512', width: '1em', height: '1em', style: { color: '#6495ed' } }, props), [
-        h('path', {
+    return (0, vue_1.h)('svg', Object.assign({ viewBox: '0 0 512 512', width: '1em', height: '1em', style: { color: '#6495ed' } }, props), [
+        (0, vue_1.h)('path', {
             fill: 'currentColor',
             d: 'M460.115 373.846l-6.941-4.008c-5.546-3.202-7.564-10.177-4.661-15.886 32.971-64.838 31.167-142.731-5.415-205.954-36.504-63.356-103.118-103.876-175.8-107.701C260.952 39.963 256 34.676 256 28.321v-8.012c0-6.904 5.808-12.337 12.703-11.982 83.552 4.306 160.157 50.861 202.106 123.67 42.069 72.703 44.083 162.322 6.034 236.838-3.14 6.149-10.75 8.462-16.728 5.011z'
         })
     ]);
 };
 function FaSolidCheckCircle(props) {
-    return h('svg', Object.assign({ width: '1em', height: '1em', viewBox: '0 0 512 512', style: { color: '#39c5bb' } }, props), [
-        h('path', {
+    return (0, vue_1.h)('svg', Object.assign({ width: '1em', height: '1em', viewBox: '0 0 512 512', style: { color: '#39c5bb' } }, props), [
+        (0, vue_1.h)('path', {
             d: 'M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256S119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z',
             fill: 'currentColor'
         })
     ]);
 }
 function FaSolidInfoCircle(props) {
-    return h('svg', Object.assign({ width: '1em', height: '1em', viewBox: '0 0 512 512', style: { color: '#6495ed' } }, props), [
-        h('path', {
+    return (0, vue_1.h)('svg', Object.assign({ width: '1em', height: '1em', viewBox: '0 0 512 512', style: { color: '#6495ed' } }, props), [
+        (0, vue_1.h)('path', {
             d: 'M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42s-42-18.804-42-42s18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z',
             fill: 'currentColor'
         })
     ]);
 }
 function FaSolidExclamationCircle(props) {
-    return h('svg', Object.assign({ width: '1em', height: '1em', viewBox: '0 0 512 512', style: { color: '#ffc107' } }, props), [
-        h('path', {
+    return (0, vue_1.h)('svg', Object.assign({ width: '1em', height: '1em', viewBox: '0 0 512 512', style: { color: '#ffc107' } }, props), [
+        (0, vue_1.h)('path', {
             d: 'M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zm-248 50c-25.405 0-46 20.595-46 46s20.595 46 46 46s46-20.595 46-46s-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z',
             fill: 'currentColor'
         })
     ]);
 }
 function styleInject(css) {
+    if (!css || typeof document === 'undefined') {
+        return;
+    }
     const head = document.head || document.getElementsByTagName('head')[0];
     const style = document.createElement('style');
     style.appendChild(document.createTextNode(css));
@@ -61,11 +67,11 @@ const styles = {
 };
 styleInject(css_248z);
 const Icon = {
-    success: h(FaSolidCheckCircle, null),
-    error: h(FaSolidTimesCircle, null),
-    info: h(FaSolidInfoCircle, null),
-    warning: h(FaSolidExclamationCircle, null),
-    loading: h(LoadingIcon, { class: styles['rotate'] })
+    success: (0, vue_1.h)(FaSolidCheckCircle, null),
+    error: (0, vue_1.h)(FaSolidTimesCircle, null),
+    info: (0, vue_1.h)(FaSolidInfoCircle, null),
+    warning: (0, vue_1.h)(FaSolidExclamationCircle, null),
+    loading: (0, vue_1.h)(LoadingIcon, { class: styles['rotate'] })
 };
 const isServerSide = typeof window === 'undefined';
 let containerNode;
@@ -89,9 +95,9 @@ const getContainerNode = () => {
 };
 const Message = (props) => {
     const { type, message, duration } = props;
-    const innerWrap = h('div', { class: styles['inner-wrap'] }, [
-        h(Icon[type], { class: styles['icon'] }),
-        h('div', { class: styles['message'] }, [h('span', null, message)])
+    const innerWrap = (0, vue_1.h)('div', { class: styles['inner-wrap'] }, [
+        (0, vue_1.h)(Icon[type], { class: styles['icon'] }),
+        (0, vue_1.h)('div', { class: styles['message'] }, [(0, vue_1.h)('span', null, message)])
     ]);
     setTimeout(() => {
         if (!innerWrap.el || duration === Infinity) {
@@ -99,10 +105,10 @@ const Message = (props) => {
         }
         innerWrap.el.classList.add(styles['disappear']);
     }, duration || 2500);
-    return h('div', { class: styles['wrap'] }, [innerWrap]);
+    return (0, vue_1.h)('div', { class: styles['wrap'] }, [innerWrap]);
 };
 //@ts-ignore
-export const message = {};
+exports.message = {};
 const types = [
     'success',
     'error',
@@ -111,7 +117,7 @@ const types = [
     'loading'
 ];
 types.forEach(type => {
-    message[type] = (content, duration = 2500) => {
+    exports.message[type] = (content, duration = 2500) => {
         return new Promise(resolve => {
             if (isServerSide) {
                 return {
@@ -142,7 +148,7 @@ types.forEach(type => {
                 const container = getContainerNode();
                 const fragment = document.createElement('div');
                 const messageVNode = Message({ type: type, duration: reallyduration, message: message });
-                render(messageVNode, fragment);
+                (0, vue_1.render)(messageVNode, fragment);
                 let isDestroyed = false;
                 const destory = () => {
                     if (isDestroyed) {
--- index.ts
+++ index.ts
@@ -104,6 +104,9 @@ function FaSolidExclamationCircle(props: any) {
 }
 
 function styleInject(css: string) {
+	if (!css || typeof document === 'undefined') {
+		return
+	}
 	const head = document.head || document.getElementsByTagName('head')[0]
 	const style = document.createElement('style')
 	style.appendChild(document.createTextNode(css))
